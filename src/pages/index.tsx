ac5a60055b34ce1b40b0d1780dddcbd4117e44fb
Evailson Barbosa <evailson@agenciaeplus.com.br>
2021-04-24 10:34:16 -0300

    Passing the props hot fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Header from "../components/Header";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
+  
   return (
     <div>
       index
     </div>
   )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      teste: 'teste'
+    }
+  }
 }
